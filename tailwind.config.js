86f5d4b1203a367830b3a2ff670439c261a36d21
peexthree <linedrop@yandex.ru>
2025-09-02 17:46:04 +0300

    Обновил код проекта

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,23 @@
 const colors = require("tailwindcss/colors");
 
 module.exports = {
-  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
 
   theme: {
     extend: {
       colors: {
+        // Нейтральные оттенки
         trueGray: colors.neutral,
+
+        // 🔹 Добавляем свой небесный цвет для аквацентра
+        aqua: {
+          light: "#E0F7FA", // очень светлый
+          DEFAULT: "#87CEEB", // небесно-голубой
+          dark: "#00BFFF", // насыщенный голубой
+        },
       },
     },
   },
@@ -14,6 +25,5 @@ module.exports = {
     require("@tailwindcss/forms"),
     require("@tailwindcss/typography"),
     require("@tailwindcss/aspect-ratio"),
-
   ],
 };

4f43fd76ff9c3ed3f157db92b810e1fc1e586aee
peexthree <linedrop@yandex.ru>
2025-09-02 15:08:31 +0300

    Обновил код проекта

--- components/benefits.js
+++ components/benefits.js
@@ -17,6 +17,7 @@ function Benefits({ data, imgPos = "left" }) {
           alt="Benefits"
           placeholder="blur"
           loading="lazy"
+          className="w-full h-auto"
         />
       </div>
 
@@ -42,28 +43,3 @@ function Benefits({ data, imgPos = "left" }) {
             ))}
           </div>
         </div>
-      </div>
-    </Container>
-  );
-}
-
-function Benefit({ title, icon, children }) {
-  return (
-    <div className="flex items-start mt-8 space-x-3">
-      <div className="flex items-center justify-center flex-shrink-0 mt-1 bg-indigo-500 rounded-md w-11 h-11">
-        {icon &&
-          React.cloneElement(icon, {
-            className: "w-7 h-7 text-indigo-50",
-          })}
-      </div>
-      <div>
-        <h4 className="text-xl font-medium text-gray-800 dark:text-gray-200">
-          {title}
-        </h4>
-        <p className="mt-1 text-gray-500 dark:text-gray-400">{children}</p>
-      </div>
-    </div>
-  );
-}
-
-export default React.memo(Benefits);
--- components/testimonials.js
+++ components/testimonials.js
@@ -56,13 +56,14 @@ export default function Testimonials() {
 function Avatar(props) {
   return (
     <div className="flex items-center mt-8 space-x-3">
-      <div className="flex-shrink-0 overflow-hidden rounded-full w-14 h-14">
+       <div className="relative flex-shrink-0 overflow-hidden rounded-full w-14 h-14">
         <Image
           src={props.image}
-          width={40}
-          height={40}
+           fill
+          className="object-cover"
+          sizes="56px"
           alt="Avatar"
-          layout="responsive"
+        
         />
       </div>
       <div>

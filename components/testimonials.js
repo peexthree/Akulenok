582feb7c794bd8b7d1c687c687d09d335d0b4a72
peexthree <linedrop@yandex.ru>
2025-09-03 19:41:43 +0300

    ╨Ю╨▒╨╜╨╛╨▓╨╕╨╗ ╨║╨╛╨┤ ╨┐╤А╨╛╨╡╨║╤В╨░

--- components/testimonials.js
+++ components/testimonials.js
@@ -1,47 +1,21 @@
 import React from "react";
-import Image from "next/image";
-import Container from "./container";
-import { motion } from "framer-motion";
-
-const testimonials = [
-  {
-    image: "/img/user1.jpg",
-    quote: "Мой сын перестал бояться воды уже после первых занятий.",
-  },
-  {
-    image: "/img/user2.jpg",
-    quote: "Дочка с радостью бежит в бассейн каждую неделю.",
-  },
-  {
-    image: "/img/user3.jpg",
-    quote: "Инструкторы внимательные и добрые, нам очень нравится.",
-  },
-];
-
 
 export default function Testimonials() {
   return (
-   <Container>
-      <div className="grid gap-8 md:grid-cols-3 sm:grid-cols-1">
-        {testimonials.map((t, i) => (
-          <motion.div
-            key={i}
-            className="flex flex-col items-center text-center p-4"
-            initial={{ opacity: 0, y: 20 }}
-            whileInView={{ opacity: 1, y: 0 }}
-            viewport={{ once: true }}
-          >
-            <Image
-              src={t.image}
-              alt="Отзыв"
-              width={80}
-              height={80}
-              className="rounded-full object-cover"
-            />
-            <p className="mt-4 text-sm text-gray-600 max-w-xs">"{t.quote}"</p>
-          </motion.div>
-        ))}
+ <div className="w-full">
+      <div
+         className="w-full max-w-2xl mx-auto h-[80vh] overflow-y-scroll"
+        style={{ WebkitOverflowScrolling: "touch" }}
+      >
+        <iframe
+          src="https://yandex.ru/maps-reviews-widget/125018811972?comments"
+           className="w-full h-[1200px]"
+          style={{ border: "none" }}
+          frameBorder="0"
+           scrolling="yes"
+          title='Отзывы о нас с Яндекса'
+        ></iframe>
       </div>
-     </Container>
+       </div>
   );
 }

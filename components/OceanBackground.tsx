734efa06247e160b6f6d71609bfbe3c38edff2ea
peexthree <linedrop@yandex.ru>
2025-09-03 14:56:47 +0300

    ╨Ю╨▒╨╜╨╛╨▓╨╕╨╗ ╨║╨╛╨┤ ╨┐╤А╨╛╨╡╨║╤В╨░

--- components/OceanBackground.tsx
+++ components/OceanBackground.tsx
@@ -3,7 +3,8 @@ import React from "react";
 const OceanBackground = () => (
   <div
     aria-hidden="true"
-    className="fixed inset-0 -z-10 bg-[#E6F7FB] bg-[url('/ocean-bg.webp')] bg-cover bg-no-repeat bg-center bg-fixed"
+    className="fixed inset-0 -z-10 bg-[#E6F7FB] bg-cover bg-no-repeat bg-center bg-fixed"
+    style={{ backgroundImage: "url('/ocean-bg.webp')" }}
   />
 );
 
--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import { ThemeProvider } from "next-themes";
 import "../css/tailwind.css";
 import { Inter, Fredoka } from "next/font/google";
 import Layout from "../components/layout";
+
 export const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 export const fredoka = Fredoka({
   subsets: ["latin"],
@@ -18,4 +19,4 @@ function MyApp({ Component, pageProps }) {
   );
 }
 
-export default MyApp;
+ export default MyApp;

6012134d10c26e0887b3edfc83eedeb38567c3cf
peexthree <linedrop@yandex.ru>
2025-09-03 17:10:42 +0300

    ╨Ю╨▒╨╜╨╛╨▓╨╕╨╗ ╨║╨╛╨┤ ╨┐╤А╨╛╨╡╨║╤В╨░

--- components/layout.tsx
+++ components/layout.tsx
@@ -6,7 +6,7 @@ export default function Layout({ children }) {
     <div className="relative">
       <OceanBackground />
        {/* Полупрозрачный слой с размытием для улучшения читаемости текста */}
-      <div className="relative z-10 backdrop-blur-md bg-white/20 dark:bg-aqua-dark/15">
+      <div className="relative z-10 backdrop-blur-md bg-white/30 dark:bg-aqua-dark/25">
         {children}
       </div>
     </div>

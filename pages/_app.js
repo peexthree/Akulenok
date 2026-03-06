7fe0cba1e25f76e6b04ef1c5c3535866da4765d0
peexthree <linedrop@yandex.ru>
2026-03-06 05:09:30 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,36 @@
 import "../css/tailwind.css";
 import { Nunito } from "next/font/google";
 import Layout from "../components/layout";
+import { SharkProvider } from "../components/SharkProvider"; // Глобальные пасхалки
+import Head from "next/head";
 
 export const nunito = Nunito({
   subsets: ["cyrillic", "latin"],
   variable: "--font-nunito",
-  weight: ["400", "500", "600", "700", "800"]
+  weight: ["400", "500", "600", "700", "800", "900"], // Добавил 900 для black-заголовков
+  display: 'swap',
 });
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <main className={`${nunito.variable} font-nunito`}>
-        <Component {...pageProps} />
-      </main>
-    </Layout>
+    <>
+      <Head>
+        {/* Оптимизация под мобильные флагманы (S25 Ultra) */}
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5" />
+        <meta name="theme-color" content="#7dd3fc" />
+      </Head>
+
+      {/* 1. SharkProvider теперь снаружи — пасхалки работают ВЕЗДЕ.
+         2. Шрифт применяется к обертке, которая включает в себя Layout (Navbar/Footer).
+      */}
+      <SharkProvider>
+        <div className={`${nunito.variable} font-nunito antialiased text-slate-900`}>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </div>
+      </SharkProvider>
+    </>
   );
 }
 

5079790143bc35c9fc545cd6bc341e34b9d1b5b2
peexthree <210461512+peexthree@users.noreply.github.com>
2026-04-01 17:05:27 +0000

    Fix video poster 404 and remove Yandex script query param

--- pages/_app.js
+++ pages/_app.js
@@ -83,7 +83,7 @@ function MyApp({ Component, pageProps }) {
                   m[i].l=1*new Date();
                   for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
                   k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
-              })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=108276956', 'ym');
+              })(window, document,'script','https://mc.yandex.ru/metrika/tag.js', 'ym');
 
               ym(108276956, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});
             `
